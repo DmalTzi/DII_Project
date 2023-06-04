1b17500545bc8e51db4a8f4aa1dd25fc3df6448f
DmalTzi_DII <Audyomg@gmail.com>
2023-06-04 10:37:45 +0700

    login update

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -34,8 +34,65 @@ router.get('/login',(req,res)=>{
     res.render('login')
 })
 
+router.post('/signin',(req,res)=>{
+    console.log(req.body)
+    const username = req.body.username
+    const password = req.body.password
+    const timeExpire = 30000  
+    console.log(username)   
+    console.log(password)
+
+    if(username === "admin" && password==="123"){
+        req.session.username = username
+        req.session.password = password
+        req.session.login = true
+        req.session.cookie.maxAge=timeExpire
+        res.redirect('/manage')
+    }
+})
+
 router.get('/addform',(req,res)=>{
     res.render("form")
+})
+
+router.get('/manage',(req,res)=>{
+    Product.find().exec((err,doc)=>{
+        res.render('manage',{product:doc})
+    })
+})
+
+router.get('/delete/:id',(req,res)=>{
+    Product.findByIdAndDelete(req.params.id,{useFindAndModify:false}).exec(err=>{
+        if(err) console.log(err)
+        res.redirect('/manage')
+    })
+})
+
+router.post('/edit',(req,res)=>{
+    const edit_id = req.body.edit_id
+    Product.findOne({_id:edit_id}).exec((err,doc)=>{
+        res.render('edit',{product:doc})
+    })
+})
+
+router.post('/update',(req,res)=>{
+    const update_id = req.body.update_id
+    let data = {
+        name:req.body.ProductName,
+        price:req.body.ProductPrice,
+        detail:req.body.ProductDetail,
+        rating:req.body.ProductRating,
+        catagory:req.body.ProductCatagory,
+    }
+    console.log(data)
+    console.log(update_id)
+    Product.findByIdAndUpdate(update_id,data,{useFindAndModify:true}).exec(err=>{
+        res.redirect('/manage')
+    })
+})
+
+router.post("/delete/:id",(req,res)=>{
+    Product.findById()
 })
 
 router.post('/insert',upload.single("ProductImage") ,(req,res)=>{
